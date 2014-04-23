d8ad52f03832eda79d310e840d726cd6dc254f3f
Geoff Hackett <ghackett@gmail.com>
2014-04-22 22:38:21 -0400

    add a test

---
+++ chop-core/src/test/java/com/episode6/hackit/chop/ChopTestTestRunner.java
@@ -0,0 +1,19 @@
+package com.episode6.hackit.chop;
+
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+import org.mockito.MockitoAnnotations;
+
+public class ChopTestTestRunner extends BlockJUnit4ClassRunner {
+
+  public ChopTestTestRunner(Class<?> klass) throws InitializationError {
+    super(klass);
+  }
+
+  @Override
+  protected Object createTest() throws Exception {
+    Object test = super.createTest();
+    MockitoAnnotations.initMocks(test);
+    return test;
+  }
+}
---
+++ chop-core/src/test/java/com/episode6/hackit/chop/DefaultTaggerTest.java
@@ -0,0 +1,38 @@
+package com.episode6.hackit.chop;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(ChopTestTestRunner.class)
+public class DefaultTaggerTest {
+
+  @Mock Chop.Tree mTree;
+
+  @Before
+  public void setUp() {
+    when(mTree.supportsLevel(any(Chop.Level.class)))
+        .thenReturn(true);
+  }
+
+  @Test
+  public void testOuterClass() {
+    Chop.plantTree(mTree);
+    String expectedTag = DefaultTaggerTest.class.getSimpleName();
+
+    Chop.e("Test Message");
+
+    verify(mTree).chopLog(
+        any(Chop.Level.class),
+        eq(expectedTag),
+        anyString()
+    );
+  }
+}
