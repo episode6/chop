a4ac9e9e8947d2175ec8ec495dd4d3e6fbda14ba
Geoff Hackett <ghackett@gmail.com>
2018-09-05 21:04:19 -0400

    add javadocs to the new public class and method

--- chop-slf4j/src/main/kotlin/com/episode6/hackit/chop/slf4j/Slf4jTree.kt
+++ chop-slf4j/src/main/kotlin/com/episode6/hackit/chop/slf4j/Slf4jTree.kt
@@ -4,9 +4,17 @@ import com.episode6.hackit.chop.Chop
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+/**
+ * An implementation of [Chop.Tree] that writes to an slf4j [Logger]
+ */
 open class Slf4jTree(private val logger: Logger) : Chop.Tree {
 
   companion object {
+
+    /**
+     * Convenience method to create a new [Slf4jTree] with a new [Logger]
+     * from slf4j's [LoggerFactory.getLogger]
+     */
     @JvmStatic
     fun create(loggerId: String): Slf4jTree = Slf4jTree(LoggerFactory.getLogger(loggerId))
   }
